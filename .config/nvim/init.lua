489599e502c040af7b00a66f88ac60788950c0b8
Nathan Davey <nathan.davey@nixs.com>
2025-03-06 14:22:04 +0100

    add starship.toml config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -857,27 +857,63 @@ require('lazy').setup({
     end,
   },
 
+  -- # --------Colors (carbonfox)-------- #
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'ellisonleao/gruvbox.nvim',
+    'EdenEast/nightfox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = true,
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'gruvbox'
+      vim.cmd.colorscheme 'carbonfox'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
   },
+  -- # --------Colors (carbonfox)-------- #
+
+  -- # --------Colors (Gruvbox dark)-------- #
+  -- {
+  --   'ellisonleao/gruvbox.nvim',
+  --   priority = 1000,
+  --   config = true,
+  --   init = function()
+  --     vim.cmd.colorscheme 'gruvbox'
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
+  -- # --------Colors (Gruvbox dark)-------- #
+
+  -- # --------Colors (synthwave_84)-------- #
+  -- {
+  --   'maxmx03/fluoromachine.nvim',
+  --   priority = 1000,
+  --   config = function()
+  --     local fm = require 'fluoromachine'
+  --     fm.setup {
+  --       glow = false,
+  --       theme = 'fluoromachine',
+  --       transparent = true,
+  --     }
+  --     vim.cmd.colorscheme 'fluoromachine'
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
+  -- # --------Colors (synthwave_84)-------- #
 
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
 
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
