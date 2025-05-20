ae24b81c5616efd488aa631f97eacfce6c325dbe
sprockmonty <sprockmonty@gmail.com>
2025-05-20 12:49:07 +0200

    use arch base-devel

--- build/start.sh
+++ build/start.sh
@@ -5,7 +5,11 @@ if ! flatpak -h ; then
 	echo "flatpak should be installed on host, install this first before starting container"
 	exit 1
 fi
-docker build --tag 'arch-dev' .
+if docker -h ; then 
+	docker build --tag 'arch-dev' .
+else
+	podman build --tag 'arch-dev' .
+fi
 
 # If you need true root access, run create with --root
 # with systemd: distrobox create --init --name arch-dev --image arch-dev --additional-packages "systemd"
